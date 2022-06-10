f06bdcfe154fddb98258f4c1ae5e14ad313b3c1c
Leonardo Juventino <leonjv08@gmail.com>
2022-06-09 22:15:06 -0500

    cambiar metodo de el formulario de Post a Get

--- form.php
+++ form.php
@@ -1,12 +1,12 @@
 <?php
 
-$nombre = $_POST['nombre'];
-$mail = $_POST['correo'];
-$mensaje = $_POST['mensaje'];
+$nombre = $_GET['nombre'];
+$mail = $_GET['correo'];
+$mensaje = $_GET['mensaje'];
 
 $mensaje = "Este mensaje fue enviado por " . $nombre . ".\r\n";
 $mensaje .= "Su e-mail es: " . $mail . " \r\n";
-$mensaje .= "El mensaje es: " . $_POST['mensaje'] . " \r\n";
+$mensaje .= "El mensaje es: " . $_GET['mensaje'] . " \r\n";
 
 $para = 'leonjv08@gmail.com';
 $asunto = 'Este mail fue enviado desde LJV-Blog';
