0ed1d844cf049aa0474669605804f8b3da9e0e72
Leonardo Juventino <leonjv08@gmail.com>
2022-06-10 20:05:19 -0500

    Imagen de hold click en la galeria de desktop para hacer más intuitivo

---
+++ form.php
@@ -0,0 +1,18 @@
+<?php
+
+$nombre = $_POST['nombre'];
+$mail = $_POST['correo'];
+$mensaje = $_POST['mensaje'];
+
+$mensaje = "Este mensaje fue enviado por " . $nombre . ".\r\n";
+$mensaje .= "Su e-mail es: " . $mail . " \r\n";
+$mensaje .= "El mensaje es: " . $_POST['mensaje'] . " \r\n";
+
+$para = 'leonjv08@gmail.com';
+$asunto = 'Este mail fue enviado desde LJV-Blog';
+
+mail($para, $asunto, utf8_decode($mensaje), $header);
+
+header('Location:index.html');
+
+?>
